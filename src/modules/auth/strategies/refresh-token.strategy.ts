62591604413b82debd124de3acc2e1ed68944186
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2026-08-18 15:28:53 +0100

    Made some changes

--- src/modules/auth/strategies/refresh-token.strategy.ts
+++ src/modules/auth/strategies/refresh-token.strategy.ts
@@ -30,7 +30,10 @@ export class RefreshTokenStrategy extends PassportStrategy(
     });
   }
 
-  async validate(req: Request, payload: { sub: string; email: string }) {
+  async validate(
+    req: Request,
+    payload: { sub: string; email: string; sessionId: string },
+  ) {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
